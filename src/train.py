99c6348b0e19b66b0561f5e52e4b2321683db5d9
charlie <charlesjones9991@gmail.com>
2021-09-07 23:57:45 +0100

    first working version

--- src/train.py
+++ src/train.py
@@ -4,31 +4,49 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.utilities.seed import seed_everything
 
-# import lightning module
-# import datamodule
+from model.resnet import MNISTResNetModule
+from datasets.MNIST_kaggle import MNISTKaggleModule
 
 seed_everything(1)
 
 
 def main(args):
 
-    logger = TensorBoardLogger("runs")
-
     dict_args = vars(args)
 
-    model = LightningModule(**dict_args)
-
-    data = DataModule(batch_size=2)
+    if args.model_name == "mnist":
+        model = MNISTResNetModule(**dict_args)
+        data = MNISTKaggleModule(batch_size=32)
+    # TODO: implement other models
+    else:
+        raise Exception("Unknown model name.")
 
     trainer = pl.Trainer.from_argparse_args(args)
+
+    if dict_args["auto_lr_find"] == True:
+        trainer.tune(model, data)
+
     trainer.fit(model, data)
 
 
 if __name__ == "__main__":
 
     parser = ArgumentParser()
-    parser = LightningModule.add_model_specific_args(parser)
+
+    # figure out which model to use
+    parser.add_argument(
+        "--model_name", type=str, default="mnist", help="mnist, titanic, or housing"
+    )
+
     parser = pl.Trainer.add_argparse_args(parser)
+
+    # THIS LINE IS KEY TO PULL THE MODEL NAME
+    temp_args, _ = parser.parse_known_args()
+
+    # let the model add what it wants
+    if temp_args.model_name == "mnist":
+        parser = MNISTResNetModule.add_model_specific_args(parser)
+
     args = parser.parse_args()
 
     main(args)
