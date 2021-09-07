413a650640794f7c814300a9d88a43310291b1aa
charlie <charlesjones9991@gmail.com>
2021-09-07 11:30:46 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
-    version='0.0.0',
-    description='Describe Your Cool Project',
-    author='',
-    author_email='',
+    name="Kaggle-Knowledge-Competitions",
+    version="0.0.0",
+    description="Solution to the three knowledge competitions on Kaggle. Implemented in Pytorch-Lightning.",
+    author="C Jones",
+    author_email="",
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url='https://github.com/PyTorchLightning/pytorch-lightning-conference-seed',
-    install_requires=['pytorch-lightning'],
+    url="https://github.com/Charl-AI/Kaggle-Knowledge-Competitions",
+    install_requires=["pytorch-lightning"],
     packages=find_packages(),
 )
-
