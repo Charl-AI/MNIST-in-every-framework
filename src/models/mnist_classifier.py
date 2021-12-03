fc715d73378172380a326acb9b60436d7e9f99a3
Charles Jones <cj1917@ic.ac.uk>
2021-12-03 21:37:26 +0000

    update arguments for new argparse usage

--- src/models/mnist_classifier.py
+++ src/models/mnist_classifier.py
@@ -2,6 +2,7 @@ import torch
 import pytorch_lightning as pl
 import torch.nn.functional as F
 import torchmetrics.functional as TF
+from pytorch_lightning.utilities.argparse import add_argparse_args, from_argparse_args
 
 from models.base_models.resnet import ResNet50
 
@@ -9,9 +10,13 @@ from models.base_models.resnet import ResNet50
 class MNISTClassifierModule(pl.LightningModule):
     """LightningModule implementation of a ResNet for MNIST"""
 
-    def __init__(self, lr: float):
+    def __init__(self, learning_rate: float = 0.01):
+        """
+        Args:
+            learning_rate (float, optional): Learning rate of optimiser. Defaults to 0.01.
+        """
         super().__init__()
-        self.lr = lr
+        self.lr = learning_rate
         self.save_hyperparameters()
         self.net = ResNet50(in_channels=1, out_classes=10)
 
@@ -44,18 +49,26 @@ class MNISTClassifierModule(pl.LightningModule):
         self.log("validation/loss", outs["loss"])
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
-        return torch.argmax(self(batch), dim=1)
+        imgs = batch  # no labels in competition test data
+        return imgs, torch.argmax(self(imgs), dim=1)
 
     def configure_optimizers(self):
         return torch.optim.SGD(self.parameters(), lr=self.lr)
 
     @classmethod
-    def add_argparse_args(cls, parent_parser):
-        parser = parent_parser.add_argument_group(cls.__name__)
-        parser.add_argument(
-            "--learning_rate",
-            help="learning rate of optimiser",
-            type=float,
-            default=0.01,
-        )
-        return parent_parser
+    def add_argparse_args(cls, parent_parser, **kwargs):
+        """Extends existing argparse by default `LightningDataModule` attributes.
+        Uses the __init__ arguments and docstring to make the agument group."""
+        return add_argparse_args(cls, parent_parser, **kwargs)
+
+    @classmethod
+    def from_argparse_args(cls, args, **kwargs):
+        """Create an instance from CLI arguments.
+
+        Args:
+            args: The parser or namespace to take arguments from. Only known arguments will be
+                parsed and passed to the :class:`~pytorch_lightning.core.datamodule.LightningDataModule`.
+            **kwargs: Additional keyword arguments that may override ones in the parser or namespace.
+                These must be valid LightningModule arguments.
+        """
+        return from_argparse_args(cls, args, **kwargs)
