bbe607cd9f6710752f77466070e09aba7cb78437
Charles Jones <cj1917@ic.ac.uk>
2021-12-20 15:44:06 +0000

    use resnet18 and remove unused other architectures

--- lightning-knowledge-competitions/models/mnist_classifier.py
+++ lightning-knowledge-competitions/models/mnist_classifier.py
@@ -1,10 +1,36 @@
-import torch
 import pytorch_lightning as pl
+import torch
 import torch.nn.functional as F
 import torchmetrics.functional as TF
 from pytorch_lightning.utilities.argparse import add_argparse_args, from_argparse_args
+from torchvision.models.resnet import BasicBlock, ResNet
 
-from models.base_models.resnet import ResNet50
+
+class ResNet18(ResNet):
+    """ResNet 18, based on torchvision implementation [BSD 3-Clause License].
+    Modified to allow for different numbers of input channels (e.g grayscale).
+
+    If you want a pretrained model, use the official torchvision implementation
+    (pretrained models only exist for 3-channel inputs, so the channel
+    modifications made here would be useless anyway).
+
+    Input to forward method: Image Tensor, size [Bx in_channels xHxW]
+    Output of forward method: Predictions Tensor, size [Bx num_classes]
+
+    """
+
+    def __init__(self, in_channels: int = 3, out_classes: int = 10):
+        super().__init__(BasicBlock, [2, 2, 2, 2], num_classes=out_classes)
+
+        # simply change the first layer to accept the number of input channels
+        self.conv1 = torch.nn.Conv2d(
+            in_channels,
+            64,
+            kernel_size=(7, 7),
+            stride=(2, 2),
+            padding=(3, 3),
+            bias=False,
+        )
 
 
 class MNISTClassifierModule(pl.LightningModule):
@@ -18,7 +44,7 @@ class MNISTClassifierModule(pl.LightningModule):
         super().__init__()
         self.lr = learning_rate
         self.save_hyperparameters()
-        self.net = ResNet50(in_channels=1, out_classes=10)
+        self.net = ResNet18(in_channels=1, out_classes=10)
 
     def forward(self, x):
         return self.net(x)
