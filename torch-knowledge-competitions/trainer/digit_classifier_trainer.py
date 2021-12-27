6291957bb21721b691a23b4256c31a0d6337b5c9
Charles Jones <cj1917@ic.ac.uk>
2021-12-27 14:23:25 +0000

    create training functions for resnet

--- torch-knowledge-competitions/trainer/digit_classifier_trainer.py
+++ torch-knowledge-competitions/trainer/digit_classifier_trainer.py
@@ -1,33 +1,110 @@
+from typing import Type
+
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchmetrics.functional as TF
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard.writer import SummaryWriter
 from tqdm import tqdm
 
 
-def train_digit_classifier(
-    model,
-    train_loader,
-    val_loader,
-    num_epochs,
-    optimizer,
-    device,
-):
+def run_epoch(
+    model: Type[nn.Module],
+    loader: DataLoader,
+    device: torch.device,
+    epoch: int,
+    mode: str,
+    optimizer: Type[torch.optim.Optimizer] = None,
+    logger: SummaryWriter = None,
+    log_every_n_steps: int = 100,
+) -> None:
+    """Run a single epoch of training or evaluation. Results are logged to tensorboard.
 
+    Args:
+        model (Type[nn.Module]): Model to train/evaluate.
+        loader (DataLoader): Dataloader of training/evaluation data.
+        device (torch.device): Device to run the model on.
+        epoch (int): Current epoch number.
+        mode (str): "train" or "val" string to specify training or evaluation mode.
+        optimizer (Type[torch.optim.Optimizer]): optimizer to use (training mode only).
+            Defaults to None.
+        logger (SummaryWriter): Tensorboard logger to use.
+        log_every_n_steps (int): Log metrics to tensorboard every n steps.
+    """
+    assert mode in ["train", "val"], f"Mode must be either 'train' or 'val', got {mode}"
+    assert (
+        log_every_n_steps < len(loader) or logger is None
+    ), f"log_every_n_steps must be less than the length of the dataloader, got {log_every_n_steps} >= {len(loader)} for {mode} mode"
+
+    model.train() if mode == "train" else model.eval()
+    global_step = epoch * len(loader)  # total number of steps taken so far
+    total_loss = 0
+    total_accuracy = 0
+
+    for batch_idx, (imgs, targets) in enumerate(
+        tqdm(
+            loader,
+            total=len(loader),
+            desc=f"Epoch {epoch}: {mode}",
+        )
+    ):
+        optimizer.zero_grad() if mode == "train" else None
+
+        imgs, targets = imgs.to(device), targets.to(device)
+        logits = model(imgs)
+        loss = F.cross_entropy(logits, targets.long())
+        accuracy = TF.accuracy(logits, targets.int())
+
+        total_loss += loss.item()
+        total_accuracy += accuracy.item()
+
+        if mode == "train":
+            loss.backward()
+            optimizer.step()
+
+        if logger is not None and batch_idx > 0 and batch_idx % log_every_n_steps == 0:
+            # mean the loss and accuracy over the last n batches
+            logger.add_scalar(
+                f"{mode}/loss", total_loss / log_every_n_steps, global_step + batch_idx
+            )
+            logger.add_scalar(
+                f"{mode}/accuracy",
+                total_accuracy / log_every_n_steps,
+                global_step + batch_idx,
+            )
+            total_loss = 0
+            total_accuracy = 0
+
+
+def train_digit_classifier(
+    model: Type[nn.Module],
+    train_loader: DataLoader,
+    val_loader: DataLoader,
+    num_epochs: int,
+    device: torch.device,
+    optimizer: Type[torch.optim.Optimizer] = None,
+    logger: SummaryWriter = None,
+    log_every_n_steps: int = 100,
+):
+    model = model.to(device)
     for epoch in range(num_epochs):
         for mode in ["train", "val"]:
-
             loader = train_loader if mode == "train" else val_loader
 
-            for imgs, targets in tqdm(
-                loader,
-                total=len(loader),
-                desc=mode,
-            ):
-                optimizer.zero_grad()
+            # equivalent to using torch.no_grad() during validation
+            with torch.set_grad_enabled(mode == "train"):
+                run_epoch(
+                    model,
+                    loader,
+                    device,
+                    epoch,
+                    mode,
+                    optimizer=optimizer,
+                    logger=logger,
+                    log_every_n_steps=log_every_n_steps,
+                )
 
-                imgs, targets = imgs.to(device), targets.to(device)
 
-                loss.backward()
-                optimizer.step()
-
-
-def predict_digit_classifier():
+def test_digit_classifier():
     pass
