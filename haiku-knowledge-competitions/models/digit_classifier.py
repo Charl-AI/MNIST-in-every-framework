8ed56adf03b3e6f9777ac40ebfe4e43148fe9e0a
Charles Jones <cj1917@ic.ac.uk>
2022-01-03 15:59:00 +0000

    add haiku resnet

---
+++ haiku-knowledge-competitions/models/digit_classifier.py
@@ -0,0 +1,6 @@
+"""The Default Haiku ResNet requires no alteration. This file just exists for
+consistency with the other models and projects."""
+
+from haiku.nets import ResNet18
+
+__all__ = ["ResNet18"]
