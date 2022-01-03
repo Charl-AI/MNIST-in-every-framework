5e1b8d149a22a6328765f74b818c6a6d2b74aaaa
Charles Jones <cj1917@ic.ac.uk>
2022-01-03 20:31:44 +0000

    wip: start creating Haiku digit classifier

--- haiku-knowledge-competitions/datasets/kaggle_mnist.py
+++ haiku-knowledge-competitions/datasets/kaggle_mnist.py
@@ -2,6 +2,7 @@ import os
 from typing import Optional
 import pandas as pd
 import numpy as np
+import jax.numpy as jnp
 from torch.utils.data import Dataset
 
 
@@ -29,16 +30,16 @@ class KaggleMNIST(Dataset):
 
         if self.train:
             df = pd.read_csv(os.path.join(data_dir, "train.csv"))
-            self.labels = df["label"].values
+            self.labels = jnp.array(df["label"].values)
             self.imgs = df.drop(labels="label", axis=1)
-            self.imgs = self.imgs.values
+            self.imgs = jnp.array(self.imgs.values)
 
         else:
             df = pd.read_csv(os.path.join(data_dir, "test.csv"))
-            self.imgs = df.values
+            self.imgs = jnp.array(df.values)
 
-        self.imgs = np.reshape(self.imgs, (-1, 28, 28))
-        self.imgs = np.expand_dims(self.imgs, axis=1)
+        self.imgs = jnp.reshape(self.imgs, (-1, 28, 28))
+        self.imgs = jnp.expand_dims(self.imgs, axis=1)
 
     def __len__(self) -> int:
         return len(self.imgs)
--- haiku-knowledge-competitions/models/digit_classifier.py
+++ haiku-knowledge-competitions/models/digit_classifier.py
@@ -1,6 +1,16 @@
-"""The Default Haiku ResNet requires no alteration. This file just exists for
-consistency with the other models and projects."""
-
+import haiku as hk
+import jax.numpy as jnp
 from haiku.nets import ResNet18
 
-__all__ = ["ResNet18"]
+
+def _forward(
+    imgs: jnp.ndarray,
+    is_training: bool,
+) -> jnp.ndarray:
+    """Forward application of the resnet."""
+    net = ResNet18(10, resnet_v2=False)
+    return net(imgs, is_training=is_training)
+
+
+# Transform our forwards function into a pair of pure functions.
+forward = hk.transform_with_state(_forward)
