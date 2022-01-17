1b3fbf767eca0b57518f852ae208a36734589f9f
Charles Jones <cj1917@ic.ac.uk>
2022-01-17 20:37:18 +0000

    add numpy dataloader shim

--- flax-knowledge-competitions/datasets/kaggle_mnist.py
+++ flax-knowledge-competitions/datasets/kaggle_mnist.py
@@ -2,8 +2,7 @@ import os
 from typing import Optional
 import pandas as pd
 import numpy as np
-import jax.numpy as jnp
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 
 class KaggleMNIST(Dataset):
@@ -56,3 +55,45 @@ class KaggleMNIST(Dataset):
             label = self.labels[index]
             return img, label
         return img
+
+
+def _numpy_collate(batch):
+    """Utility function for dataloding numpy arrays into JAX."""
+    if isinstance(batch[0], np.ndarray):
+        return np.stack(batch)
+    elif isinstance(batch[0], (tuple, list)):
+        transposed = zip(*batch)
+        return [_numpy_collate(samples) for samples in transposed]
+    else:
+        return np.array(batch)
+
+
+class NumpyLoader(DataLoader):
+    """Shim of Pytorch DataLoader that loads numpy arrays into JAX."""
+
+    def __init__(
+        self,
+        dataset,
+        batch_size=1,
+        shuffle=False,
+        sampler=None,
+        batch_sampler=None,
+        num_workers=0,
+        pin_memory=False,
+        drop_last=False,
+        timeout=0,
+        worker_init_fn=None,
+    ):
+        super(self.__class__, self).__init__(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            sampler=sampler,
+            batch_sampler=batch_sampler,
+            num_workers=num_workers,
+            collate_fn=_numpy_collate,
+            pin_memory=pin_memory,
+            drop_last=drop_last,
+            timeout=timeout,
+            worker_init_fn=worker_init_fn,
+        )
