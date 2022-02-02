52a463d7da313edec0e2eb92329344c0c722fa88
Charles Jones <cj1917@ic.ac.uk>
2022-02-02 22:02:58 +0000

    add titanic to download script

--- download_data.sh
+++ download_data.sh
@@ -6,7 +6,9 @@ cd data
 # download datasets
 kaggle competitions download -p kaggle_mnist -c digit-recognizer
 kaggle competitions download -p kaggle_tweets -c nlp-getting-started
+kaggle competitions download -p kaggle_titanic -c titanic
 
 # prepare datasets
 unzip kaggle_mnist/digit-recognizer.zip -d kaggle_mnist
 unzip kaggle_tweets/nlp-getting-started.zip -d kaggle_tweets
+unzip kaggle_titanic/titanic.zip -d kaggle_titanic
