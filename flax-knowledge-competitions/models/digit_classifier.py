96607aafc86cad933d7317ef8cd3adcd90cd6cdb
Charles Jones <cj1917@ic.ac.uk>
2022-05-13 11:15:54 +0000

    fix type checking issues

--- flax-knowledge-competitions/models/digit_classifier.py
+++ flax-knowledge-competitions/models/digit_classifier.py
@@ -15,19 +15,18 @@
 """Flax implementation of ResNet V1.
 https://github.com/google/flax/blob/main/examples/imagenet/models.py"""
 
-# See issue #620.
-# pytype: disable=wrong-arg-count
 
 from functools import partial
 from typing import Any, Callable, Sequence, Tuple
 
-from flax import linen as nn
+import jax
 import jax.numpy as jnp
+from flax import linen as nn
 
 ModuleDef = Any
 
 
-class ResNetBlock(nn.Module):
+class ResNetBlock(nn.module.Module):
     """ResNet block."""
 
     filters: int
@@ -36,7 +35,7 @@ class ResNetBlock(nn.Module):
     act: Callable
     strides: Tuple[int, int] = (1, 1)
 
-    @nn.compact
+    @nn.module.compact
     def __call__(
         self,
         x,
@@ -46,7 +45,7 @@ class ResNetBlock(nn.Module):
         y = self.norm()(y)
         y = self.act(y)
         y = self.conv(self.filters, (3, 3))(y)
-        y = self.norm(scale_init=nn.initializers.zeros)(y)
+        y = self.norm(scale_init=jax.nn.initializers.zeros)(y)
 
         if residual.shape != y.shape:
             residual = self.conv(self.filters, (1, 1), self.strides, name="conv_proj")(
@@ -57,37 +56,7 @@ class ResNetBlock(nn.Module):
         return self.act(residual + y)
 
 
-class BottleneckResNetBlock(nn.Module):
-    """Bottleneck ResNet block."""
-
-    filters: int
-    conv: ModuleDef
-    norm: ModuleDef
-    act: Callable
-    strides: Tuple[int, int] = (1, 1)
-
-    @nn.compact
-    def __call__(self, x):
-        residual = x
-        y = self.conv(self.filters, (1, 1))(x)
-        y = self.norm()(y)
-        y = self.act(y)
-        y = self.conv(self.filters, (3, 3), self.strides)(y)
-        y = self.norm()(y)
-        y = self.act(y)
-        y = self.conv(self.filters * 4, (1, 1))(y)
-        y = self.norm(scale_init=nn.initializers.zeros)(y)
-
-        if residual.shape != y.shape:
-            residual = self.conv(
-                self.filters * 4, (1, 1), self.strides, name="conv_proj"
-            )(residual)
-            residual = self.norm(name="norm_proj")(residual)
-
-        return self.act(residual + y)
-
-
-class ResNet(nn.Module):
+class ResNet(nn.module.Module):
     """ResNetV1."""
 
     stage_sizes: Sequence[int]
@@ -95,13 +64,13 @@ class ResNet(nn.Module):
     num_classes: int
     num_filters: int = 64
     dtype: Any = jnp.float32
-    act: Callable = nn.relu
+    act: Callable = jax.nn.relu
 
-    @nn.compact
+    @nn.module.compact
     def __call__(self, x, train: bool = True):
-        conv = partial(nn.Conv, use_bias=False, dtype=self.dtype)
+        conv = partial(nn.linear.Conv, use_bias=False, dtype=self.dtype)
         norm = partial(
-            nn.BatchNorm,
+            nn.normalization.BatchNorm,
             use_running_average=not train,
             momentum=0.9,
             epsilon=1e-5,
@@ -112,8 +81,8 @@ class ResNet(nn.Module):
             self.num_filters, (7, 7), (2, 2), padding=[(3, 3), (3, 3)], name="conv_init"
         )(x)
         x = norm(name="bn_init")(x)
-        x = nn.relu(x)
-        x = nn.max_pool(x, (3, 3), strides=(2, 2), padding="SAME")
+        x = jax.nn.relu(x)
+        x = nn.pooling.max_pool(x, (3, 3), strides=(2, 2), padding="SAME")
         for i, block_size in enumerate(self.stage_sizes):
             for j in range(block_size):
                 strides = (2, 2) if i > 0 and j == 0 else (1, 1)
@@ -125,14 +94,9 @@ class ResNet(nn.Module):
                     act=self.act,
                 )(x)
         x = jnp.mean(x, axis=(1, 2))
-        x = nn.Dense(self.num_classes, dtype=self.dtype)(x)
+        x = nn.linear.Dense(self.num_classes, dtype=self.dtype)(x)
         x = jnp.asarray(x, self.dtype)
         return x
 
 
 ResNet18 = partial(ResNet, stage_sizes=[2, 2, 2, 2], block_cls=ResNetBlock)
-ResNet34 = partial(ResNet, stage_sizes=[3, 4, 6, 3], block_cls=ResNetBlock)
-ResNet50 = partial(ResNet, stage_sizes=[3, 4, 6, 3], block_cls=BottleneckResNetBlock)
-ResNet101 = partial(ResNet, stage_sizes=[3, 4, 23, 3], block_cls=BottleneckResNetBlock)
-ResNet152 = partial(ResNet, stage_sizes=[3, 8, 36, 3], block_cls=BottleneckResNetBlock)
-ResNet200 = partial(ResNet, stage_sizes=[3, 24, 36, 3], block_cls=BottleneckResNetBlock)
